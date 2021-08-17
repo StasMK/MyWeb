d0f6b17680a798be58ad6beb0f77d0e9ef4477fd
StanislavUIT <klibashev.s@beldor.centr.by>
2021-08-18 00:40:58 +0300

    1

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -10,6 +10,9 @@ class uch_roads(models.Model):
     nomt2 = models.CharField(max_length=10)
     dl = models.DecimalField(max_digits=7, decimal_places=3)
     history = models.TextField()
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    data_greated = models.DateTimeField(auto_now_add=True)
+    data_update = models.DateTimeField(auto_now=True)
     god =models.SmallIntegerField()
 
 class sp_roads(models.Model):
@@ -25,16 +28,3 @@ class kat(models.Model):
     k_km = models.DecimalField(max_digits=7, decimal_places=3)
     kat = models.CharField(max_length=3)
 
-
-# Description: 	E.2.1 Номер и титул автомобильной дороги
-# Name	Type	Size
-# sys_nom	Длинное целое	4
-# sys_nom_gis	Длинное целое	4
-# name	Длинное целое	4
-# km	Одинарное с плавающей точкой	4
-# k_km	Одинарное с плавающей точкой	4
-# nomt1	Короткий текст	10
-# nomt2	Короткий текст	10
-# dl	Одинарное с плавающей точкой	4
-# history	Длинный текст	-
-# god	Целое	2
